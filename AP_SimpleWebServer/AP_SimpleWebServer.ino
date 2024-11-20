a07f6b6d033c6292bb3503d13f12a939766a27d0
gbrsni <72036069+gbrsni@users.noreply.github.com>
2024-11-20 15:37:42 +0100

    Stop waiting for serial

--- AP_SimpleWebServer/AP_SimpleWebServer.ino
+++ AP_SimpleWebServer/AP_SimpleWebServer.ino
@@ -35,9 +35,9 @@ typedef struct {
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  // while (!Serial) {
+  //   ; // wait for serial port to connect. Needed for native USB port only
+  // }
 
   Serial.println("Access Point Web Server");
 

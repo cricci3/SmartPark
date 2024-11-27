810811ead0de0e8bdd57e19bc68f71f63af748f1
cricci3 <100707767+cricci3@users.noreply.github.com>
2024-11-27 14:09:21 +0100

    wifi + smartpark

--- WiFiWebClient/WiFiWebClient.ino
+++ WiFiWebClient/WiFiWebClient.ino
@@ -1,23 +1,24 @@
-/*
-  Web client
-
- This sketch connects to a website (http://example.com) using the WiFi module.
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * Arduino Portenta H7
-
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 31 May 2012
- by Tom Igoe
- */
-
+#include <Wire.h>
+#include "Adafruit_VL53L0X.h"
 #include <WiFi.h>
 
 #include "arduino_secrets.h" 
+
+#define TCA9548A_ADDR 0x70
+
+//PIN Custom for led
+#define R1 8
+#define G1 7
+#define B1 6
+#define R2 5
+#define G2 4
+#define B2 3
+#define R3 2
+#define G3 1
+#define B3 0
+
+WiFiClient client;
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
@@ -28,7 +29,86 @@ int status = WL_IDLE_STATUS;
 // use the numeric IP instead of the name for the server:
 IPAddress server(10, 0, 0, 1);  // IP address for example.com (no DNS)
 
-WiFiClient client;
+
+// to avoid 10000 lines of code we decide to create a structure
+struct RGBLed {
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+};
+
+const RGBLed rgbLeds[] = {
+    {R1, G1, B1},
+    {R2, G2, B2},
+    {R3, G3, B3}
+};
+
+// LED 0 - Sensor 0 - Bus 0 : green
+// LED 1 - Sensor 1 - Bus 2 : yellow
+// LED 2 - Sensor 2 - Bus 6 : purple
+
+
+const uint8_t sensorBuses[] = {0, 2, 6};
+const uint8_t numSensors = sizeof(sensorBuses) / sizeof(sensorBuses[0]);
+
+Adafruit_VL53L0X sensors[numSensors];
+
+// select the bus
+void TCA9548A_Select(uint8_t bus) {
+    Wire.beginTransmission(TCA9548A_ADDR);
+    Wire.write(1 << bus);
+    Wire.endTransmission();
+}
+
+// general function to set the led 
+void setLEDColor(int ledIndex, int color) {
+    switch(ledIndex) {
+        case 0:  // LED 1
+            switch(color) {
+                case 0:  // Verde 
+                    digitalWrite(R1, LOW);
+                    digitalWrite(G1, HIGH);
+                    digitalWrite(B1, LOW);
+                    break;
+                case 1:  // Rosso 
+                    digitalWrite(R1, HIGH);
+                    digitalWrite(G1, LOW);
+                    digitalWrite(B1, LOW);
+                    break;
+            }
+            break;
+            
+        case 1:  // LED 2 : fundamental to use analogwrite
+            switch(color) {
+                case 0:  // giallo
+                    analogWrite(R2, 204);
+                    analogWrite(G2, 136);
+                    analogWrite(B2, 0);
+                    break;
+                case 1:  // rosso
+                    analogWrite(R2, 255);
+                    analogWrite(G2, 0);
+                    analogWrite(B2, 0);
+                    break;
+            }
+            break;
+            
+        case 2:  // LED 3
+            switch(color) {
+                case 0:  // blu 
+                    analogWrite(R3, 149);
+                    analogWrite(G3, 0);
+                    analogWrite(B3, 179);
+                    break;
+                case 1:  // rosso
+                    analogWrite(R3, 255);
+                    analogWrite(G3, 0);
+                    analogWrite(B3, 0);
+                    break;
+            }
+            break;
+    }
+}
 
 void sendPostRequest(int floorID, int stallType, int counter, IPAddress server) {
     String jsonPayload = "{\"floor_id\": "+String(floorID) +
@@ -48,61 +128,6 @@ void sendPostRequest(int floorID, int stallType, int counter, IPAddress server) 
     client.println();
 }
 
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
-
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 3 seconds for connection:
-    delay(3000);
-  }
-  Serial.println("Connected to wifi");
-  printWifiStatus();
-
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-
-    sendPostRequest(1, 2, 0, server);
-  }
-}
-
-void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
-  }
-}
-
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -118,4 +143,116 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial) delay(1);
+    Wire.begin();
+
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("Communication with WiFi module failed!");
+      // don't continue
+      while (true);
+    }
+
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
+
+      // wait 3 seconds for connection:
+      delay(3000);
+    }
+    Serial.println("Connected to wifi");
+    printWifiStatus();
+
+    Serial.println("\nStarting connection to server...");
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+      Serial.println("connected to server");
+
+      sendPostRequest(1, 2, 0, server);
+    }
+    
+    // Inizializzazione pin LED
+    for (uint8_t i = 0; i < numSensors; i++) {
+        pinMode(rgbLeds[i].R, OUTPUT);
+        pinMode(rgbLeds[i].G, OUTPUT);
+        pinMode(rgbLeds[i].B, OUTPUT);
+        setLEDColor(i, 0);  // Imposta il colore default per ogni LED
+    }
+    
+    // Inizializzazione sensori
+    for (uint8_t i = 0; i < numSensors; i++) {
+        TCA9548A_Select(sensorBuses[i]);
+        Serial.print("Inizializzazione sensore ");
+        Serial.print(i);
+        Serial.print(" su bus ");
+        Serial.println(sensorBuses[i]);
+        
+        if (!sensors[i].begin()) {
+            Serial.print("ERRORE: Sensore ");
+            Serial.print(i);
+            Serial.println(" non inizializzato!");
+            while (1);
+        } else {
+            Serial.print("Sensore ");
+            Serial.print(i);
+            Serial.println(" OK");
+        }
+    }
+}
+
+void loop() {
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }
+
+    // if the server's disconnected, stop the client:
+    if (!client.connected()) {
+      Serial.println();
+      Serial.println("disconnecting from server.");
+      client.stop();
+
+      // do nothing forevermore:
+      while (true);
+    }
+
+    for (uint8_t i = 0; i < numSensors; i++) {
+        TCA9548A_Select(sensorBuses[i]);
+        VL53L0X_RangingMeasurementData_t measure;
+        sensors[i].rangingTest(&measure, false);
+        
+        Serial.print("Sensore ");
+        Serial.print(i);
+        Serial.print(" (Bus ");
+        Serial.print(sensorBuses[i]);
+        Serial.print("): ");
+        
+        if (measure.RangeStatus != 4) {
+            int distance = measure.RangeMilliMeter;
+            Serial.print("Distanza: ");
+            Serial.print(distance);
+            Serial.println(" mm");
+            
+            if (distance > 0 && distance <= 70) {
+                setLEDColor(i, 1);  // Rosso quando oggetto vicino
+            } else {
+                setLEDColor(i, 0);  // Colore default quando oggetto lontano
+            }
+        } else {
+            Serial.println("Fuori portata");
+            setLEDColor(i, 0);  // Colore default quando fuori portata
+        }
+        
+        Serial.println("-------------------");
+    }
+    delay(100);
 }
